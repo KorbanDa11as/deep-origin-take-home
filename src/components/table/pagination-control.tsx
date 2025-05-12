c69aad45b7431533ee6a75d821a578fd6d656a62
Ilia Talalai <ijatalalai@gmail.com>
2025-05-11 22:14:11 -0700

    add paginated/server data table

---
+++ src/components/table/pagination-control.tsx
@@ -0,0 +1,30 @@
+export type PaginationControlProps = {
+  page: number
+  pageSize: number
+  totalRowCount: number
+  setPage: React.Dispatch<React.SetStateAction<number>>
+}
+export function PaginationControl({ page, pageSize, totalRowCount, setPage }: PaginationControlProps) {
+
+  const upperBoundForRowCount = pageSize * (page + 1);
+  const lowerBoundForRowCount = pageSize * page + 1;
+  const rowIndexRange = [
+    lowerBoundForRowCount,
+    upperBoundForRowCount > totalRowCount ? totalRowCount : upperBoundForRowCount,
+  ];
+
+  const upperBoundPage = Math.ceil(totalRowCount / pageSize)
+  const dataRange = `${rowIndexRange[0]}-${rowIndexRange[1]}`;
+  return <div className="pagination">
+    {totalRowCount > 0 && (
+      <span >
+        <b>{dataRange}</b> of <b>{totalRowCount}</b>
+      </span>
+    )}
+    <button onClick={() => setPage(prev => prev -= 1)} disabled={page === 0}>prev </button>
+    <span>{page + 1}</span>
+    <button onClick={() => setPage(prev => prev += 1)} disabled={page === upperBoundPage - 1}>next </button>
+
+
+  </div>
+}
---
+++ src/components/table/server-table.tsx
@@ -0,0 +1,59 @@
+
+import React, { Fragment, ReactElement, useEffect, useState } from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+
+import { Table } from './table';
+import {
+  useReactTable,
+  getCoreRowModel,
+  getExpandedRowModel,
+  ColumnDef,
+  flexRender,
+} from '@tanstack/react-table'
+import { GenericDataEndpoint, Task } from '../../api/methods'
+import { buttonClasses } from '@mui/material';
+import { PaginationControl } from './pagination-control';
+
+type TableProps<TData> = {
+  fetchData: (page: number, pageSize: number) => Promise<GenericDataEndpoint<Task> | null>
+  columns: ColumnDef<TData>[]
+}
+
+export function ServerTable({
+  fetchData,
+  columns,
+}: TableProps<Task>): ReactElement {
+  const [pageData, setPageData] = useState<Task[]>([])
+  const [page, setPage] = useState(0)
+  const [error, setError] = useState<Error | null>(null)
+  const [rowCount, setRowCount] = useState(0)
+
+  async function fetchPageData() {
+    const resp = await fetchData(page, 10)
+    if (resp === null) {
+      setError(new Error('failed to fetch'))
+    }
+    else {
+      const { data, total } = resp as GenericDataEndpoint<Task>
+      setPageData(data as Task[])
+      // setRowCount(total)
+    }
+  }
+  useEffect(() => {
+    fetchPageData()
+  }, [page])
+
+
+  if (error) return <div>{error.message}</div>
+  return <>
+    <Table
+      data={pageData} columns={columns} updateData={fetchPageData}></Table>
+    <PaginationControl page={page} pageSize={10} totalRowCount={rowCount} setPage={setPage} />
+
+  </>
+
+}
+
+
